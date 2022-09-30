835c04987be525ea1f70e02eb572750b292fbf01
Bhargavi <lakshmibarghavi04@gmail.com>
2022-09-29 19:13:27 -0700

    Added customValidationMethod

--- Common/Validation.cs
+++ Common/Validation.cs
@@ -2,6 +2,16 @@
 {
     public class Validation
     {
-
+        public bool customValidation()
+        {
+			try
+			{
+				return true;
+			}
+			catch (Exception exp)
+			{
+				return false;
+			}
+        }
     }
 }
