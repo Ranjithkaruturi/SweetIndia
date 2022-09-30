9ba9f1ab4548b44f0eeb6eec62ad16cbf000e697
Bhargavi <lakshmibarghavi04@gmail.com>
2022-09-29 17:09:34 -0700

    test

--- SweetModel/MainModel.cs
+++ SweetModel/MainModel.cs
@@ -3,7 +3,7 @@
     public class MainModel
     {
         public int Item_Code { get; set; } = 0;
-        public string Item_Name{ get; set; } 
+        public string Item_Name { get; set; } = "";
 
     }
 }
